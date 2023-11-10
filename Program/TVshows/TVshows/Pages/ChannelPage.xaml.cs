8139f763dc882a442c54c6241686052846e9f296
Alexey Verbitskiy <104301476+Biflez48@users.noreply.github.com>
2023-11-10 20:56:40 +0300

    Add files via upload

--- Program/TVshows/TVshows/Pages/ChannelPage.xaml.cs
+++ Program/TVshows/TVshows/Pages/ChannelPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,43 @@ namespace TVshows.Pages
     /// </summary>
     public partial class ChannelPage : Page
     {
+        ObservableCollection<TileItem> ChannelsTiles = new ObservableCollection<TileItem>();
         public ChannelPage()
         {
             InitializeComponent();
+            ShowTiles();
+        }
+
+        private class TileItem
+        {
+            public string NameCh { get; set; }
+            public string DescCh { get; set; }
+        }
+
+        private void ShowTiles()
+        {
+            ObservableCollection<Database.Channels> Channels =
+               new ObservableCollection<Database.Channels>(
+                   Core.Database.Channels
+                   .Where(C => C.NameCh!="" && C.DescriptionCh!=""));
+
+            for (int i = 0; i < Channels.Count(); i++)
+            {
+                TileItem tile = new TileItem();
+                tile.NameCh = Channels[i].NameCh;
+                tile.DescCh = Channels[i].DescriptionCh;
+                ChannelsTiles.Add(tile);
+            }
+
+            ChnlsListBox.ItemsSource = ChannelsTiles;
         }
 
         private void ShowScheduleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChnlsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
