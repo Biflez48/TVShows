84c86aaea2f0cc463b3300617ed957f527a5cdc9
Alexey Verbitskiy <104301476+Biflez48@users.noreply.github.com>
2023-11-10 20:55:50 +0300

    Add files via upload

--- Program/TVshows/TVshows/Database/TVShows.Designer.cs
+++ Program/TVshows/TVshows/Database/TVShows.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\4is3\TVshows\TVshows\Database\TVShows.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\student.COLLEGE.007\Downloads\TVshows-20231110T130251Z-001\TVshows\TVshows\Database\TVShows.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
